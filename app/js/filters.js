ff4d172fce75c1128079994129c889c9b9415671
flaky <flakyfilibuster@gmail.com>
2014-03-19 09:41:16 +0100

    add custom filter for completed todo items | start work on filter for categories

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,8 +2,36 @@
 
 /* Filters */
 
-angular.module('todocatFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
+var todocatFilters = angular.module('todocatFilters', []);
+
+todocatFilters.filter('status', function() {
+  return function(todos, scope) {
+
+      function isCompleted(todo) {
+          return !!todo.completed;
+      }
+
+      function isUncompleted(todo) {
+          return !todo.completed;
+      }
+
+      if(typeof todos === "object") {
+          switch (scope.status) {
+              case "completed":
+                  return todos.filter(isCompleted);
+              case "uncompleted":
+                  return todos.filter(isUncompleted);
+              case "all":
+                  return todos;
+              default:
+                  return todos;
+          }
+      }
+  };
+});
+
+
+todocatFilters.filter('category', function() {
+  return function(todos, scope) {
   };
 });
