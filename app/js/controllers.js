38107a7f56bfb68db3ff11d5b3d90183a0012c68
flaky <flakyfilibuster@gmail.com>
2014-03-19 09:29:12 +0100

    change existing functions to play nice with new service promises | add logic for detail view, saving notes and setting a todo item to completed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,29 +5,57 @@
 var todocatControllers = angular.module('todocatControllers', []);
 
 todocatControllers.controller('TodoListCtrl', ['$scope', 'Todo', function($scope, Todo) {
-    $scope.orderProp = 'name';
-    $scope.formData = {};
 
-    Todo.query(function(data) {
-        $scope.todos = data;
-        $scope.$apply();
-    })
+    $scope.orderProp = 'name';
+    $scope.status = 'all';
+    $scope.formData = {};
+    $scope.selected = null;
+
+    Todo.getAll().then(function(data) {
+            $scope.todos = data;
+            $scope.selected = data[0];
+    });
 
     $scope.createTodo = function() {
-        Todo.save($scope.formData);
-        $scope.formData = {};
-        Todo.query(function(data) {
-            $scope.todos = data;
-            $scope.$apply();
-        })
+        Todo.save($scope.formData).then(function() {
+            Todo.getAll().then(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+            });
+        });
     };
 
     $scope.deleteTodo = function(target) {
-        Todo.delete(target);
+        Todo.delete(target)
+            .then(Todo.getAll()
+            .then(function(data) {
+                $scope.todos = data;
+        }));
     };
-}]);
 
-todocatControllers.controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todo', function($scope, $routeParams, Todo) {
-    Todo.get({_id: $routeParams.todoId}, function(data) {
-    })
+    $scope.todoDetail = function(target) {
+       $scope.selected = target;
+    };
+
+    $scope.saveNotes = function(todo, element) {
+        var note = element.target.value.replace(/\r?\n/g, '<br/>');
+        Todo.update(todo, note)
+            .then(Todo.getAll()
+            .then(function(data) {
+                $scope.todos = data;
+        }));
+    };
+
+    $scope.completeTodo = function(target) {
+        Todo.complete(target)
+            .then(Todo.getAll()
+            .then(function(data) {
+                if ($scope.selected._id === target._id) {
+                    Todo.get(target).then(function(rsp) {
+                        $scope.selected = rsp;
+                    });
+                }
+            $scope.todos = data;
+        }));
+    };
 }]);
