b0a449f6f980a90235ae6c260d79e7127f3a005f
flaky <flakyfilibuster@gmail.com>
2014-03-19 15:54:41 +0100

    add filter for todo categories

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,34 +5,44 @@
 var todocatFilters = angular.module('todocatFilters', []);
 
 todocatFilters.filter('status', function() {
-  return function(todos, scope) {
+    return function(todos, scope) {
 
-      function isCompleted(todo) {
-          return !!todo.completed;
-      }
+        function isCompleted(todo) {
+            return !!todo.completed;
+        }
 
-      function isUncompleted(todo) {
-          return !todo.completed;
-      }
+        function isUncompleted(todo) {
+            return !todo.completed;
+        }
 
-      if(typeof todos === "object") {
-          switch (scope.status) {
-              case "completed":
-                  return todos.filter(isCompleted);
-              case "uncompleted":
-                  return todos.filter(isUncompleted);
-              case "all":
-                  return todos;
-              default:
-                  return todos;
-          }
-      }
-  };
+        if(typeof todos === "object") {
+            switch (scope.status) {
+                case "completed":
+                    return todos.filter(isCompleted);
+                case "uncompleted":
+                    return todos.filter(isUncompleted);
+                case "all":
+                    return todos;
+                default:
+                    return todos;
+            }
+        }
+    };
 });
 
-
 todocatFilters.filter('category', function() {
-  return function(todos, scope) {
-      return todos;
-  };
+    return function(todos, scope) {
+
+        if(scope.category === 'all') {
+            return todos;
+        }
+
+        function isCategory(todo) {
+            return (scope.category === todo.category);
+        }
+
+        if(typeof todos === "object") {
+            return todos.filter(isCategory);
+        }
+    };
 });
