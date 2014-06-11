57855760d3551695e15aed25d551a088e7e2f75b
flaky <flakyfilibuster@gmail.com>
2014-06-11 11:30:13 +0200

    Refactoring
    - refactor controllers
    - utilize promise chaining

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,58 +2,51 @@
 
 /* Controllers */
 
-var todocatControllers = angular.module('todocatControllers', []);
+var todoControllers = angular.module('todoControllers', [])
 
-todocatControllers.controller('TodoListCtrl', ['$scope', 'Todo', '$window', function($scope, Todo, $window) {
+.controller('TodoListCtrl', ['$scope', 'Todo', '$window', function TodoListCtrl($scope, Todo) {
 
-    // get nodewebkit window object
-    $scope.nwWin = $window.gui.Window.get();
-
+    // set up some default variables
     $scope.orderProp = 'name';
     $scope.status = 'all';
     $scope.formData = {};
     $scope.selected = null;
 
-    Todo.getAll().then(function(data) {
-        $scope.todos = data;
-        $scope.selected = data[0];
-    });
-
-    $scope.createTodo = function() {
-        Todo.save($scope.formData).then(function() {
-            Todo.getAll().then(function(data) {
-                $scope.formData.name = '';
-                $scope.todos = data;
+    var loadAllTodos = function() {
+        return Todo.getAll()
+            .then(function(allTodos) {
+            $scope.todos = allTodos;
+                return allTodos;
             });
-        });
     };
 
-    $scope.deleteTodo = function(target) {
-        Todo.getSlaves(target).then(function(slaves) {
-            if(slaves.length > 0) {
-                Todo.updateSlaves(target, slaves[0]._id, slaves.length-1)
-                .then(Todo.delete(target)
-                    .then(Todo.getAll()
-                    .then(function(data) {
-                        $scope.todos = data;
-                        if ($scope.selected) {
-                            Todo.getSlaves(slaves[0]._id).then(function(data) {
-                                $scope.slaves = data;
-                            });
-                        }
-                })));
-            } else {
-                Todo.delete(target)
-                    .then(Todo.getAll()
-                    .then(function(data) {
-                        $scope.todos = data;
-                }));
+    var loadAllSlaves = function(masterId) {
+        return Todo.getSlaves(masterId)
+            .then(function(allSlaves) {
+                $scope.slaves = allSlaves;
+                return allSlaves;
+            });
+    };
+
+    $scope.createTodo = function() {
+        Todo.save($scope.formData)
+            .then(loadAllTodos)
+            .then(function(allTodos) {
+                $scope.formData.name = '';
+                $scope.todos = allTodos;
+                return allTodos;
+            });
+    };
+
+    $scope.deleteTodo = function(targetId) {
+        loadAllSlaves(targetId).then(function(slaves) {
+            if (slaves.length > 0) {
+                Todo.updateSlaves(targetId, slaves[0]._id, slaves.length-1);
             }
+            Todo.delete(targetId).then(loadAllTodos);
         });
         if ($scope.selected) {
-                
-
-            if(target === $scope.selected._id) {
+            if(targetId === $scope.selected._id) {
                 $scope.selected = null;
                 $scope.slaves = null;
             }
@@ -61,49 +54,30 @@ todocatControllers.controller('TodoListCtrl', ['$scope', 'Todo', '$window', func
     };
 
     $scope.todoDetail = function(target) {
-       $scope.selected = target;
-       Todo.getSlaves(target._id).then(function(data) {
-           $scope.slaves = data;
-       })
+        $scope.selected = target;
+        loadAllSlaves(target._id);
     };
 
     $scope.saveNotes = function(todo, element) {
         var note = element.target.value.replace(/\r?\n/g, '<br/>');
-        Todo.update(todo, note)
-            .then(Todo.getAll()
-            .then(function(data) {
-                $scope.todos = data;
-        }));
+        Todo.updateNote(todo._id, note).then(loadAllTodos);
     };
 
     $scope.slaveTask = function(masterId, slaveId) {
-        Todo.slaveIt(masterId, slaveId).then(function() {
-            Todo.getAll().then(function(data) {
-                $scope.todos = data;
-                if ($scope.selected) {
-                    if(masterId === $scope.selected._id) {
-                        Todo.getSlaves(masterId).then(function(data) {
-                            $scope.slaves = data;
-                        });
-                    }
-                }
-            });
-        });
+        Todo.slaveIt(masterId, slaveId).then(loadAllTodos);
     };
 
     $scope.completeTodo = function(target) {
-        if(target.completed) {
-            return;
-        }
+        (target.completed) ||
         Todo.complete(target)
-            .then(Todo.getAll()
-            .then(function(data) {
-                if ($scope.selected._id === target._id) {
-                    Todo.get(target).then(function(rsp) {
-                        $scope.selected = rsp;
-                    });
-                }
-            $scope.todos = data;
-        }));
+            .then(loadAllTodos);
+
+        if ($scope.selected._id === target._id) {
+            Todo.get(target).then(function(rsp) {
+                $scope.selected = rsp;
+            });
+        }
     };
+
+    loadAllTodos();
 }]);
